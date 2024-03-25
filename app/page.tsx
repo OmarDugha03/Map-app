93804119a6bbc982bbb5a15b4bed933f36d8ca89
OmarDugha <116832203+OmarDugha03@users.noreply.github.com>
2024-03-25 14:57:08 +0300

    feat: Implement interactive map with React Leaflet
    - Added 'Map' component to render an interactive map using React Leaflet.
    - Integrated map functionalities such as markers and popups.
    - Implemented minimap feature for overview and navigation.
    - Added event handling for map interaction.
    - Styling adjustments for map display.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+'use client'
+import dynamic from 'next/dynamic'
+
 export default function Home() {
-  return <main></main>;
+  const Map = dynamic(() => import('@/components/Map'), {
+    loading: () => <p>loading...</p>,
+    ssr: false
+  })
+  return (
+    <>
+      <nav>
+        <h1>Hello World !</h1>
+      </nav>
+      <main className='container  flex h-full w-full items-center justify-center'>
+        <Map />
+      </main>
+    </>
+  )
 }
